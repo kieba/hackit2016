2d8b1ffcaf29a2e2e1338176fddf76429389603f
kieba <kieba4@gmail.com>
2016-11-26 18:43:24 +0100

    update Model

--- src/model/PollPartOption.java
+++ src/model/PollPartOption.java
@@ -1,4 +1,37 @@
-package Model;
+package model;
 
 public class PollPartOption {
+    private PollPart parrent;
+    private int optionId;
+    private String description;
+
+    public PollPartOption(PollPart parrent, int optionId, String description) {
+        this.parrent = parrent;
+        this.optionId = optionId;
+        this.description = description;
+    }
+
+    public PollPart getParrent() {
+        return parrent;
+    }
+
+    public void setParrent(PollPart parrent) {
+        this.parrent = parrent;
+    }
+
+    public int getOptionId() {
+        return optionId;
+    }
+
+    public void setOptionId(int optionId) {
+        this.optionId = optionId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
